341ea63c7b8889b117500e4e1a5d983f27606e09
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-01-15 20:31:00 +0100

    Full page background

--- src/invitation/invitation.js
+++ src/invitation/invitation.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./invitation.css";
+
+import background from "./invitation-background.jpg";
 
 const Invitation = (props) => {
 	return (
-		<div>
-			<h1>You've been invited!</h1>
-		</div>
+		<>
+			<div className="fullPageBackground" style={{ backgroundImage: `url(${background})` }} />		
+			<div className="invitation">
+				<h1>Invitation</h1>
+			</div>
+		</>
 	);
 }
 
