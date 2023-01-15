25d508da16592b0c8f46f14a0b8b65b937165866
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-01-15 21:04:45 +0100

    Frame and postCard design

--- src/invitation/invitation.js
+++ src/invitation/invitation.js
@@ -6,9 +6,34 @@ import background from "./invitation-background.jpg";
 const Invitation = (props) => {
 	return (
 		<>
-			<div className="fullPageBackground" style={{ backgroundImage: `url(${background})` }} />		
-			<div className="invitation">
-				<h1>Invitation</h1>
+			<div className="fullPageBackground" style={{ backgroundImage: `url(${background})` }} />
+			<div className="postalCard">
+				<div className="postalCard__frame">
+					<div className="postalCard__content">
+						<div className="postalCard__content__one">
+							<span>Save the date</span>
+						</div>
+						<div className="postalCard__content__two">
+							<span>Fanny & Nathaniel</span>
+						</div>
+						<div className="postalCard__content__three">
+							<span>Are getting married</span>
+						</div>
+						<div className="postalCard__content__four">
+							<span>June.29.2024</span>
+						</div>
+						<div className="postalCard__content__five">
+							<span>Stockholm, Sweden</span>
+						</div>
+						<div className="postalCard__content__six">
+							{/* <Countdown /> */}
+							<span>365 : 24 : 43 : 48</span>
+						</div>
+						<div className="postalCard__content__seven">
+							<span>Formal Invitation to follow</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		</>
 	);
