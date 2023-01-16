7318e5e3279c0446123ae9795216d673a9c59efd
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-01-16 13:08:14 +0100

    convert to tsx

--- src/invitation/countdown.tsx
+++ src/invitation/countdown.tsx
@@ -1,28 +1,28 @@
 import React from "react";
 
-const Countdown = (props) => {
+const Countdown = () => {
   // Create a date object for the date june 29 2024
-  const weddingDate = new Date("June 29, 2024 00:00:00");
+  const weddingDate: number = new Date("June 29, 2024 00:00:00").getTime();
 
   // The countdown should be updated every second
   setInterval(() => {
     // Get the current date and time
-    const now = new Date().getTime();
+    const now: number = new Date().getTime();
 
     // Find the distance between now and the wedding date
-    const distance = weddingDate - now;
+    const distance: number = weddingDate - now;
 
     // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const days: number = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours: number = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes: number = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds: number = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Update the divs with the id countdown__days__number, countdown__hours__number, countdown__minutes__number, countdown__seconds__number with the correct values
-    document.getElementById("countdown__days__number").innerHTML = days;
-    document.getElementById("countdown__hours__number").innerHTML = hours;
-    document.getElementById("countdown__minutes__number").innerHTML = minutes;
-    document.getElementById("countdown__seconds__number").innerHTML = seconds;
+    // Bug: Object is possibly 'null'
+    document.getElementById("countdown__days__number")!.innerHTML = days.toString();
+    document.getElementById("countdown__hours__number")!.innerHTML = hours.toString().padStart(2, "0");
+    document.getElementById("countdown__minutes__number")!.innerHTML = minutes.toString().padStart(2, "0");
+    document.getElementById("countdown__seconds__number")!.innerHTML = seconds.toString().padStart(2, "0");
 
   }, 1000);
 
