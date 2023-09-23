ae659d99751dbfa05a24340d1f038a29af7333ba
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-09-23 09:56:04 +0200

    Remove login route

--- src/App.tsx
+++ src/App.tsx
@@ -11,39 +11,23 @@ import OurStory from './Pages/OurStory/OurStory';
 import WeddingDay from './Pages/WeddingDay/WeddingDay';
 import FAQ from './Pages/QuestionAnswer/QuestionAnswer';
 import Itinerary from './Pages/Itinerary/Itinerary';
+import Stockholm from './Pages/Stockholm/Stockholm';
 
 function App() {
-  const [login, setLogin] = React.useState(false);
-  const [loading, setLoading] = React.useState(true);
-
-  useEffect(() => {
-    const loginStatus = localStorage.getItem('login');
-    if (loginStatus) {
-      setLogin(true);
-    }
-    setLoading(false);
-  }, []);
-
   return (
     <div className="App">
-      {!login && !loading && (
-        <LoginPage setLogin={(login: boolean) => setLogin(login)} />
-      )}
-      {login && (
-        <>
-          {window.location.pathname !== '/invitation' && <Navigation />}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/invitation" element={<Invitation />} />
-            <Route path="/gifts" element={<Gifts />} />
-            <Route path="/our-story" element={<OurStory />} />
-            <Route path="/wedding-day" element={<WeddingDay />} />
-            <Route path="/qa" element={<FAQ />} />
-            <Route path="/itinerary" element={<Itinerary />} />
-            <Route path="*" element={<h1 style={{textAlign: "center"}}>404 Not Found </h1>} />
-          </Routes>
-        </>
-      )}
+      {window.location.pathname !== '/invitation' && <Navigation />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invitation" element={<Invitation />} />
+        <Route path="/gifts" element={<Gifts />} />
+        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/wedding-day" element={<WeddingDay />} />
+        <Route path="/qa" element={<FAQ />} />
+        <Route path="/itinerary" element={<Itinerary />} />
+        <Route path="/stockholm-tips" element={<Stockholm />} />
+        <Route path="*" element={<h1 style={{textAlign: "center"}}>404 Not Found </h1>} />
+      </Routes>
     </div>
   );
 }
